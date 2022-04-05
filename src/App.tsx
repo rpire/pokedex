c17d8d6624e7370b6f57d13f7275d665a031d3c8
rpire <rubenpire7@gmail.com>
2022-04-05 18:15:12 -0400

    Corregir errores de ESLint en index.tsx y App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import { FunctionComponent } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <>
-      <h1>Pokédex</h1>
-      <p>Bestiario de Pokémon</p>
-    </>
-  );
-}
+const App: FunctionComponent = () => (
+  <>
+    <h1>Pokédex</h1>
+    <p>Bestiario de Pokémon</p>
+  </>
+);
 
 export default App;
