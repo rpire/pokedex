2819c540f8438e4e7629c2466ad9cb987965cb4c
rpire <rubenpire7@gmail.com>
2022-04-07 10:35:15 -0400

    Refactorizar componentes y estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Home from './components/pages/home/Home';
 import Pokedex from './components/pages/pokedex/Pokedex';
 import ErrorPage from './components/pages/error/ErrorPage';
 import Navbar from './components/Navbar';
+import { fetchPokemon } from './redux/reducers/pokemon';
 
-const App: FC = () => (
-  <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="pokedex" element={<Pokedex />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
-  </>
-);
+const App: FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPokemon());
+  });
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="pokedex" element={<Pokedex />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import store from './redux/configureStore';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
 );
